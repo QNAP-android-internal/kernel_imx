9a8f63140ffe42bea80ed1df620eb93482dc2cdd
Wig Cheng <wigcheng@ieiworld.com>
2022-08-24 14:58:08 +0800

    drivers: panel-simple: workaround for SW reset on android display HAL
    This issue only happened on android system. Before run boot animation,
    android display HAL will call panel_disable -> panel_unprepare ->
    panel_prepare -> panel_enable procedure.
    
    It will cause some low cost panel cannot return back status, need
    re-initial panel, so reduce the procedure to panel_prepare ->
    panel_enable once is enough.

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -204,6 +204,11 @@ struct panel_simple {
 	enum drm_panel_orientation orientation;
 };
 
+#ifdef CONFIG_ANDROID
+	static int enable_cnt = 0;
+	static int prepare_cnt = 0;
+#endif
+
 static inline struct panel_simple *to_panel_simple(struct drm_panel *panel)
 {
 	return container_of(panel, struct panel_simple, base);
@@ -337,7 +342,9 @@ static void panel_simple_wait(ktime_t start_ktime, unsigned int min_ms)
 static int panel_simple_disable(struct drm_panel *panel)
 {
 	struct panel_simple *p = to_panel_simple(panel);
-
+#ifdef CONFIG_ANDROID
+	return 0;
+#endif
 	if (!p->enabled)
 		return 0;
 
@@ -368,7 +375,9 @@ static int panel_simple_unprepare(struct drm_panel *panel)
 	struct panel_simple *p = to_panel_simple(panel);
 	int ret;
 
-	/* Unpreparing when already unprepared is a no-op */
+#ifdef CONFIG_ANDROID
+	return 0;
+#endif
 	if (!p->prepared)
 		return 0;
 
@@ -406,7 +415,14 @@ static int panel_simple_prepare_once(struct panel_simple *p)
 	int hpd_asserted;
 	unsigned long hpd_wait_us;
 
-	panel_simple_wait(p->unprepared_time, p->desc->delay.unprepare);
+#ifdef CONFIG_ANDROID
+	if(prepare_cnt < 1)
+		prepare_cnt += 1;
+	else
+		return 0;
+#endif
+	if (p->prepared)
+		return 0;
 
 	err = regulator_enable(p->supply);
 	if (err < 0) {
@@ -504,6 +520,12 @@ static int panel_simple_enable(struct drm_panel *panel)
 {
 	struct panel_simple *p = to_panel_simple(panel);
 
+#ifdef CONFIG_ANDROID
+	if(enable_cnt < 1)
+		enable_cnt += 1;
+	else
+		return 0;
+#endif
 	if (p->enabled)
 		return 0;
 
