b7d6c672222fc0b094eb3f377e6160f31b11ed4a
Jason Yang <jasonyang@ieiworld.com>
2025-07-28 16:27:21 +0800

    media: platform: nxp: add 16bit width format data support

--- drivers/media/platform/nxp/dwc-mipi-csi2.c
+++ drivers/media/platform/nxp/dwc-mipi-csi2.c
@@ -651,7 +651,11 @@ static void dwc_csi_device_ipi_config(struct dwc_csi_device *csidev)
 	 */
 	val = dwc_csi_read(csidev, CSI2RX_IPI_MODE);
 	val &= ~CSI2RX_IPI_MODE_CONTROLLER;
-	val &= ~CSI2RX_IPI_MODE_COLOR_MODE16;
+	if (csi_fmt->width > 8) {
+		val |= CSI2RX_IPI_MODE_COLOR_MODE16;
+	} else {
+		val &= ~CSI2RX_IPI_MODE_COLOR_MODE16;
+	}
 	val |= CSI2RX_IPI_MODE_CUT_THROUGH;
 	dwc_csi_write(csidev, CSI2RX_IPI_MODE, val);
 }
