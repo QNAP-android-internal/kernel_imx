d6bfae77d5c41bfddf449adf89b4f30ead6d1aaa
Jason Yang <jasonyang@ieiworld.com>
2026-01-05 11:12:28 +0800

    gpu: drm: panel: panel-tq101aj02: fix merge errors from 6.6.23 to 6.12.34
    Change-Id: I99ad385d4cfc95fc7aca780fa99dda38b646a874

--- drivers/gpu/drm/panel/panel-tq101aj02.c
+++ drivers/gpu/drm/panel/panel-tq101aj02.c
@@ -11,7 +11,7 @@
 #include <linux/module.h>
 #include <linux/media-bus-format.h>
 #include <linux/of.h>
-#include <linux/of_platform.h>
+#include <linux/of_device.h>
 #include <linux/regulator/consumer.h>
 #include <linux/gpio.h>
 
